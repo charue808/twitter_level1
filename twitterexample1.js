ebe177bacb47cdc960c78973c0f62bd3ec544c4d
charue808 <charlesgum@gmail.com>
2014-11-25 18:29:09 -1000

    able to submit posts and removed insecure

--- twitterexample1.js
+++ twitterexample1.js
@@ -1,36 +1,68 @@
 Posts = new Mongo.Collection('posts');
 
-if (Meteor.isClient) {
-  Meteor.subscribe('posts');
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user()
 
-  Template.postsList.helpers({
-  posts: function() {
-    return Posts.find();
+    var post = _.extend(_.pick(postAttributes, 'body'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
   }
 });
+
+if (Meteor.isClient) {
+  Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
+
+  Meteor.subscribe('posts');
+
+Template.postsList.helpers({
+  posts: function() {
+    return Posts.find({}, {sort:{submitted: -1}});
+  }
+});
+
+Template.submitPost.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var post = { body: $(e.target).find('[name=message]').val() }
+
+   Meteor.call('post', post, function(error, id) {
+    if(error)
+      return alert(error.reason);
+   })
+
+    document.getElementById('textBody').value = ' ';
+
+    
+  }
+});
+
+Template.postItem.helpers({
+  submittedText: function() {
+    return  new Date(this.submitted).toString();
+  }
+});
+
 }
 
 
       
 if (Meteor.isServer) {
-  if (Posts.find().count() === 0) {
-  Posts.insert({
-    author: 'jumal@harvestlearninggroup.com',
-    body: 'Going to get boba!'
-  });
+  
 
-  Posts.insert({
-    author: 'charlesgum@gmail.com',
-    body: 'watching the game at McSorley\'s'
-  });
-
-  Posts.insert({ 
-    author: 'dirkgaines@test.com',
-    body: 'new testing data'
-  });
-       Meteor.publish('posts', function() {
+Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+}
        
         
-}
