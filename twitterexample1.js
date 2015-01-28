791abe2a07b3a45f882f3a6707d055ec3dd15b66
charue808 <charlesgum@gmail.com>
2015-01-28 01:56:54 +0000

    updated meteor to 1.04, added in images, created currentUser section

--- twitterexample1.js
+++ twitterexample1.js
@@ -20,6 +20,26 @@ if (Meteor.isClient) {
   Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
+    
+    
+Meteor.Spinner.options = {
+    lines: 13, // The number of lines to draw
+    length: 10, // The length of each line
+    width: 5, // The line thickness
+    radius: 15, // The radius of the inner circle
+    corners: 0.7, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#fff', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: true, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+};
 
 Meteor.subscribe('posts', function onReady() {
   Session.set('postsLoaded', true);
@@ -37,16 +57,7 @@ Template.postsList.helpers({
   // postsLoaded add to postList helpers
   // postloaded function returns Session postsLoaded
 });
-Template.submitPost.helpers({
-  textTotal: function() {
-    var textLength = 140;
-    return textLength;
-  },
-  textCount: function() {
 
-  },
-
-});
 
 Template.submitPost.events({
   'submit form': function(e) {
@@ -66,6 +77,22 @@ Template.postItem.helpers({
     return  new Date(this.submitted).toString();
   }
 });
+    
+Template.userFeature.events({
+  'click #button1': function () {
+     document.getElementById('theImage').src="/image/Twitterbox1.png";
+     document.getElementById('theTextBox').src="/image/wigflip-ds (2).png";
+    },
+  'click #button2': function () {
+     document.getElementById('theImage').src="/image/twitterinvite2.png";
+     document.getElementById('theTextBox').src="/image/wigflip-ds (4).png";
+     
+    },
+  'click #button3': function () {
+     document.getElementById('theImage').src="/image/twitter3.png";
+     document.getElementById('theTextBox').src="/image/wigflip-ds (3).png";
+    },
+})
 
 }
 
